e9f3d16aab9c2fdc66bc1642dfa0c623919b403c
nobuyo <world_fantasy_2017@live.jp>
2016-10-03 13:14:18 +0900

    update etc/install.sh

--- etc/install.sh
+++ etc/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#     __ _ _           
-#    / _(_) | ___  ___ 
+#     __ _ _
+#    / _(_) | ___  ___
 #   | |_| | |/ _ \/ __|
 #  _|  _| | |  __/\__ \
 # (_)_| |_|_|\___||___/
@@ -14,63 +14,77 @@ set -u
 # set -o igncr
 
 is_available() {
-	which "$1" >/dev/null 2>&1
-	return $?
+  which "$1" >/dev/null 2>&1
+  return $?
 }
 
 if [ -z "${DOTPATH:-}" ]; then
-	DOTPATH=~/.dotfiles; export DOTPATH
+  DOTPATH=~/.dotfiles; export DOTPATH
 fi
 
 DOTGIT="https://github.com/nobuyo/dotfiles.git"; export DOTGIT
 
 get_dotfile() {
-	if [ -d "$DOTPATH" ]; then
-		echo "$DOTPATH already exists"
-		exit 1
-	fi
+  if [ -d "$DOTPATH" ]; then
+    echo "$DOTPATH already exists"
+    exit 1
+  fi
 
-	if is_available "git"; then
-			git clone --recursive "$DOTGIT" "$DOTPATH"
+  if is_available "git"; then
+      git clone --recursive "$DOTGIT" "$DOTPATH"
 
-	elif is_available "curl"; then
+  elif is_available "curl"; then
 
-		local tarball="https://github.com/nobuyo/dotfiles/archive/master.tar.gz"
-		if is_available "curl"; then
-		   curl -L "$tarball"
-		fi | tar xvz
-		if [ ! -d dotfiles-master ]; then
-			log_fail "dotfiles-master: not found"
-			exit 1
-		fi
-		command mv -f dotfiles-master "$DOTPATH"
+    local tarball="https://github.com/nobuyo/dotfiles/archive/master.tar.gz"
+    if is_available "curl"; then
+       curl -L "$tarball"
+    fi | tar xvz
+    if [ ! -d dotfiles-master ]; then
+      log_fail "dotfiles-master: not found"
+      exit 1
+    fi
+    command mv -f dotfiles-master "$DOTPATH"
 
-	else
-		   echo "curl required"
-		   exit 1
-	fi
-	
-	echo "Done"
+  else
+       echo "curl required"
+       exit 1
+  fi
+
+  echo "Done"
 }
 
 deploy() {
-	echo ""
-	echo "Deploying dotfiles..."
+  echo ""
+  echo "Deploying dotfiles..."
 
-	if [ ! -d $DOTPATH ]; then
-		echo "$DOTPATH: not found"
-		exit 1
-	fi
+  if [ ! -d $DOTPATH ]; then
+    echo "$DOTPATH: not found"
+    exit 1
+  fi
 
-	cd "$DOTPATH"
+  cd "$DOTPATH"
 
-	make deploy &&
+  make deploy &&
 
-	echo "Done"
+  echo "Done"
+}
+
+check() {
+  local req=("zsh","gcc","bzip2","xz","git","aria2","make","curl","wget")
+  for $r in ${req[@]}; do
+    echo -n "$r"
+    which "$r"
+    if [ $? -eq 0 ]; then
+      echo "OK"
+    else
+      echo "Not installed"
+    fi
+  done
 }
 
 setup() {
-	get_dotfile && deploy
+  get_dotfile && deploy
 }
 
 setup
+# check
